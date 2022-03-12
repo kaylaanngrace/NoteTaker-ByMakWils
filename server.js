9448b9a0359398f6e51e5f951abce9b77aac8fbf
MakWils <kaylaanngrace@gmail.comgit remote add origin git@github.com:kaylaanngrace/horiseon-optimize-accessibility.git>
2022-03-11 23:49:45 -0600

    Add heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const fs = require('fs');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 app.listen(PORT, () => {
     console.log(`Server is listening on PORT: ${PORT}`);
